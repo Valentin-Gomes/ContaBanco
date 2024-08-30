a57f98eeb7a51e7930497074b140d0d16801f301
Valentin-Gomes <valentin_gomes@hotmail.com>
2024-08-30 10:26:35 -0300

    ContaBanco Finalizado

--- Conta_Banco/src/ContaTerminal.java
+++ Conta_Banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! ");
+        try (Scanner ler = new Scanner(System.in)) {
+            String nome;
+            String agencia;
+            int numConta;
+            double saldo;
+
+            System.out.print("Digite seu nome: ");
+            nome = ler.nextLine();
+            System.out.print("Digite a agência: ");
+            agencia = ler.nextLine();
+            System.out.print("Digite o número da conta: ");
+            numConta = ler.nextInt();
+            System.out.print("Digite seu saldo: ");
+            saldo = ler.nextDouble();
+
+            System.out.print("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numConta+ ", e seu saldo de R$ " + saldo + " já está disponível para saque");
+        }
+
+
     }
 }
